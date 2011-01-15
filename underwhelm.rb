42ec2c60a8864883bd61d53cffa6d701e8584055
Dan Dorman <dan.dorman@gmail.com>
2011-01-15 14:13:55 -0700

    Refactor so tweet-loading code works in the absence of an (initial) tweet file.

--- underwhelm.rb
+++ underwhelm.rb
@@ -18,16 +18,22 @@ class Underwhelm < Sinatra::Base
   end
 
   get '/tweet' do
-    tweet = Twitter.user_timeline("dandorman").find {|tweet| tweet.text !~ /^@/}.to_json
-    File.open("/tmp/tweet", 'w') {|f| f.write(tweet) }
+    tweet = Twitter.user_timeline("dandorman").find {|tweet| tweet.text !~ /^@/}
+    File.open(tweet_file, 'w') {|f| f.write(tweet.to_json) }
     haml :tweet, :locals => { :tweet => tweet }
   end
 
   get '/' do
-    if File.readable?("/tmp/tweet")
-      @tweet = JSON.parse(File.open("/tmp/tweet").read)
+    if File.readable?(tweet_file)
+      @tweet = Hashie::Mash.new(JSON.parse(File.open(tweet_file).read))
+    else
+      @tweet = nil
     end
     haml :index
+  end
+
+  def tweet_file
+    @tweet_file ||= File.dirname(__FILE__) + "/tmp/tweet"
   end
 
 end
